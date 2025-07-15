e246cf525d8b4b38bec4651522e079350a765279
Riskiana Wulan <riskiana.wulan@kuelap.io>
2025-07-15 09:46:14 +0700

    init

---
+++ src/main/java/dev/riskiana/app/SpringSecurityAsymmetricEncryptionApplication.java
@@ -0,0 +1,13 @@
+package dev.riskiana.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAsymmetricEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAsymmetricEncryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/riskiana/app/SpringSecurityAsymmetricEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.riskiana.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAsymmetricEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
