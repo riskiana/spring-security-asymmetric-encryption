fa18fdeaf2e92fb7b55925708fe0f63450edab68
Riskiana Wulan <riskiana.wulan@kuelap.io>
2025-07-15 10:03:00 +0700

    add jsonwebtoken and openapi dependency

--- src/main/java/dev/riskiana/app/Application.java
+++ src/main/java/dev/riskiana/app/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityAsymmetricEncryptionApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityAsymmetricEncryptionApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
